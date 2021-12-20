46039a2cae4aa787c3a0056dec7f59b2b05febfa
TVSK <tvsk101@gmail.com>
2021-12-20 09:41:50 +0300

    Добавьте файлы проекта.

---
+++ HomeWork3/MyClass.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    /// <summary>
+    /// Класс, реализующий однонаправленный одномерный список
+    /// </summary>
+    class MyList<MyType>
+    {
+        /// <summary>
+        /// Реализация Элемента списка
+        /// </summary>
+        class MyListItem
+        {
+            public MyType Value;
+            public MyListItem Next;
+        }
+        /// <summary>
+        /// Первый элемент списка
+        /// </summary>
+        MyListItem head = null;
+
+        /// <summary>
+        /// Первоначальная длина списка
+        /// </summary>
+        private int length = 0;
+
+        /// <summary>
+        /// Реализация длины списка
+        /// </summary>
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+        /// <summary>
+        /// Метод, реализующий добавление элемента в список
+        /// </summary>
+        public void Add(MyType value)
+        {
+            // Увеличиваем длину списка
+            length++;
+
+            // Если первый элемент списка отсутствует, то создаем новый элемент и присваиваем ему значение
+            if (head == null)
+            {
+                head = new MyListItem() { Value = value };
+                return;
+            }
+            MyListItem current = head;
+            // Пока следующий элемент списка не определен, сдвигаем текущий элемент вперед и присваиваем ему новое значение
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = new MyListItem() { Value = value };
+        }
+
+        /// <summary>
+        /// Метод, реализующий удаление элемента из списка по индексу
+        /// </summary>
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 & index < length & head != null)
+            {
+                // Уменьшаем длину списка
+                length--;
+
+                // Если индекс равен нулю, то список остается без изменений
+                if (index == 0)
+                {
+                    head = head.Next;
+                }
+                // Иначе перебираем список до нужного элемента
+                // current1 присваивается значение нужного элемента
+                // current2 присваивается значение следующего элемента (current1.Next)
+                // current2.Next перешагивает еще на элемент вперед
+                // current1 присваивается значение current2 
+                // Итог: искомый элемент удаляется из последовательности            
+                else
+                {
+                    MyListItem current1 = head;
+                    for (int i = 0; i < index - 1; i++)
+                    {
+                        current1 = current1.Next;
+                    }
+                    MyListItem current2 = current1.Next;
+                    current2 = current2.Next;
+                    current1.Next = current2;
+                }
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Индекс удаляемого элемента за пределами списка");
+            }
+        }
+
+        /// <summary>
+        /// Индексатор
+        /// </summary>
+        public MyType this[int index]
+        {
+            get
+            {
+                MyListItem current = head;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                return (current.Value);
+            }
+            set
+            {
+                MyListItem current = head;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                current.Value = value;
+            }
+        }
+    }
+}
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    /// <summary>
+    /// Работа программы
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Список строковых значений: ");
+            try
+            {
+                // Создаем новый экземпляр списка MyList и задаем, что он принимает строковые значения
+                MyList<string> list1 = new MyList<string>();
+
+                // Заполняем его случайными значениями (конвертируем числа в строковый тип данных)
+                for (int i = 0; i < 20; i++)
+                {
+                    list1.Add(random.Next(-10, 10).ToString());
+                }
+
+                // Выводим исходный список элементов
+                for (int i = 0; i < list1.Length; i++)
+                {
+                    Console.Write(list1[i] + " ");
+                }
+
+                Console.WriteLine();
+
+                // Удаляем случайный элемент списка (индекс может выходить за диапазон)
+                list1.RemoveAt(random.Next(-30, 30));
+
+                // Выводим измененный список
+                for (int i = 0; i < list1.Length; i++)
+                {
+                    Console.Write(list1[i] + " ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Список целочисленных значений значений: ");
+            try
+            {
+                // Создаем новый экземпляр списка MyList и задаем, что он принимает целочисленные значения
+                MyList<int> list2 = new MyList<int>();
+
+                // Заполняем его случайными значениями
+                for (int i = 0; i < 20; i++)
+                {
+                    list2.Add(random.Next(-10, 10));
+                }
+
+                // Выводим исходный список элементов
+                for (int i = 0; i < list2.Length; i++)
+                {
+                    Console.Write(list2[i] + " ");
+                }
+
+                Console.WriteLine();
+
+                // Удаляем случайный элемент списка (индекс может выходить за диапазон)
+                list2.RemoveAt(random.Next(-30, 30));
+
+                // Выводим измененный список
+                for (int i = 0; i < list2.Length; i++)
+                {
+                    Console.Write(list2[i] + " ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
